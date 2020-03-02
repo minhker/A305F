51a83f0485b196c6856822aea594cb0a9465b190
minhker <minhkk0248@gmail.com>
2020-03-02 14:38:09 +0700

    add A.K.I tools and some file change , dont need care

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 338 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#338 SMP PREEMPT Sun Mar 1 14:09:36 +07 2020"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "minhka"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.4.111-MinhKer_Pie-V12-A305F-20200301"
